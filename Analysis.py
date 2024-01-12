1753858ad333b2f65253cc6af3c16038e68132dd
Brendan Morgan <bmo_32@hotmail.com>
2024-01-12 16:28:02 -0500

    Added code to Analysis constructor function to load config file when Analysis object is instatiated

--- Analysis.py
+++ Analysis.py
@@ -1,13 +1,56 @@
+import json
+import yaml 
+import argparse
+import requests
+
+
 class Analysis:
+
+    ''' Load config into an Analysis object
+
+    Load system-wide configuration from `configs/system_config.yml`, user configuration from
+    `configs/user_config.yml`, and the specified analysis configuration file
+
+    Parameters
+    ----------
+    analysis_config : str
+        Path to the analysis/job-specific configuration file
+
+    Returns
+    -------
+    analysis_obj : Analysis
+        Analysis object containing consolidated parameters from the configuration files
+
+    Notes
+    -----
+    The configuration files should include parameters for:
+        * GitHub API token
+        * Plot color
+        * Plot title
+        * Plot x and y axis titles
+        * Figure size
+        * Default save path
+
+    '''
   
-  job_config_path='./configs/job_config.yml'
+    current_config_path='./configs/system_config.yml'
 
-  #constructor will load job-specific configuration file upon object instantiation
-  def __init__(self, analysis_config=job_config_path):
-    self.analysis_config = analysis_config
+    #constructor will load configuration file upon object instantiation
+    def __init__(self, analysis_config=current_config_path):
+        self.analysis_config = analysis_config
+
+        with open(self.analysis_config, 'r') as f:
+            self.this_config = yaml.safe_load(f)
+
+            print(self.this_config)
 
 
-  def load_data():
+
+
+
+    def load_data(self):
+        
+        print(self.this_config)
 
     ''' Retrieve data from the GitHub API
 
@@ -24,23 +67,21 @@ class Analysis:
 
     '''
 
+obj_1 = Analysis()
+obj_1.load_data()
+    # def compute_analysis() -> Any:
 
-  def compute_analysis() -> Any
+    # '''Analyze previously-loaded data.
 
-    '''Analyze previously-loaded data.
+    # This function runs an analytical measure of your choice (mean, median, linear regression, etc...)
+    # and returns the data in a format of your choice.
 
-    This function runs an analytical measure of your choice (mean, median, linear regression, etc...)
-    and returns the data in a format of your choice.
+    # Parameters
+    # ----------
+    # None
 
-    Parameters
-    ----------
-    None
+    # Returns
+    # -------
+    # analysis_output : Any
 
-    Returns
-    -------
-    analysis_output : Any
-
-    '''
-
-
-  def plot_data(save_path:Optional[str] = None) -> matplotlib.Figure
+    # '''
